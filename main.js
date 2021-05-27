4e9ad74ae198224dfd2aa4313007b551fddb1726
AeeUX <AndreasEriksen99@hotmail.com>
2021-05-28 00:32:40 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
 
 // Add active class to the current btn (highlighting the element)
-$(document).ready(function(){
-    $('ul li a').click(function(){
-      $('li a').removeClass("active");
-      $(this).addClass("active");
-  });
-  });
+
+const currentLocation = Location.href;
