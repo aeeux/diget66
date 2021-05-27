d28e0b30c5d19aec9eb75ef42fc73577a17a536c
AeeUX <AndreasEriksen99@hotmail.com>
2021-05-27 23:08:00 +0200

    Update main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+
+// Add active class to the current button (highlight it)
+var header = document.getElementById("myDIV");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
