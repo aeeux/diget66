42ec98d2ebedf37754df5f56cde8fd27f0c6c9a2
AeeUX <AndreasEriksen99@hotmail.com>
2021-05-29 01:03:19 +0200

    Update main.js

--- main.js
+++ main.js
@@ -10,3 +10,54 @@ for (let i = 0; i < menuLength; i++) {
         menuItem[i].className = 'active';
     }
 }
+
+// Gsap.js
+
+TweenMax.from("h1", 2, {
+    delay: .1,
+    y: 20,
+    opacity: 0,
+    ease: Expo.easeInOut
+})
+
+TweenMax.from("p", 2, {
+    delay: .25,
+    x: -100,
+    opacity: 0,
+    ease: Expo.easeInOut
+})
+
+TweenMax.from("img", 2, {
+    delay: .25,
+    x: 100,
+    opacity: 0,
+    ease: Expo.easeInOut
+})
+
+TweenMax.from(".menu-icon", 2, {
+    delay: .25,
+    x: -100,
+    opacity: 0,
+    ease: Expo.easeInOut
+})
+
+TweenMax.from("iframe", 2, {
+    delay: .25,
+    x: 100,
+    opacity: 0,
+    ease: Expo.easeInOut
+})
+
+TweenMax.from("button", 2, {
+    delay: .1,
+    x: 0,
+    opacity: -1,
+    ease: Expo.easeInOut
+})
+
+TweenMax.staggerFrom("ul li a", 2, {
+      delay: .01,
+      opacity: -1,
+      y: 0,
+      ease: Expo.easeInOut
+});
